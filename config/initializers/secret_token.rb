3919925995c8345f702a8e758e6e79d0834cc3b9
Anita Ranade <agranade@stcharleshealthcare.org>
2011-08-29 17:17:21 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '342edd500564b566fee78b245b6819ecb5efc6328a3260768831861b50227f55fee0a124bc94d4825d47cac21d2c7529e081aae1720fc4f1ae48da27345adfcb'
